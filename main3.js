cec51c00ddcb1594f54eff29cdbceddb202d864a
Smiley876 <anzelged16@gmail.com>
2024-12-20 09:30:54 +0200

    Add files via upload

---
+++ main3.js
@@ -0,0 +1,12 @@
+//local variables
+var a = 10;
+var b = 27;
+var c = (20 / 4);
+var x = 2 ** 5;
+var z = 5;
+var y = 25;
+
+//function
+function show(a, b, c, x, z, y){
+     document.getElementById(sum1).innerHTML = a + y - b + c + x - z;
+}
